26bd39352a9f64fdfc4aa5a5e1720011f217e0dd
Yuval Kogman <nothingmuch@woobling.org>
2014-12-13 03:46:53 +0200

    fix time quantization according to strobe period
    in this episode nothingmuch confuses the dividend and divisor yet again
    
    in addition, the remainder of the frame duration is now accounted to reduce the
    rounding errors which were clamping strobe_delta_t close to 0 effectively
    adding a constant buffer sized latency even without the aforementioned bug

--- tuner.js
+++ tuner.js
@@ -27,13 +27,13 @@ function init_strobe (canvas,stream,audioContext) {
 	var strobe_pitch = parseFloat(canvas.getAttribute("class").substr(canvas.getAttribute("class").lastIndexOf("_")+1));
 
 	// FIXME deprecated
-	var proc = audioContext.createScriptProcessor(2048, 1, 1); // FIXME samplerate/bufsize = update frequency?
+	var proc = audioContext.createScriptProcessor(1024, 1, 1); // FIXME samplerate/bufsize = update frequency?
 
 	var buffers = [ new Float32Array(proc.bufferSize), new Float32Array(proc.bufferSize) ];
 	var buffer_t = 0;
 	var last_frame = 0;
+	var last_strobe = 0;
 	var strobe_buf;
-	var strobe_delta_t = 0; // proc.bufferSize / 2 * sample_duration;
 
 	var strobe_segments = strobe_width;
 
@@ -41,6 +41,8 @@ function init_strobe (canvas,stream,audioContext) {
 	var sample_rate = audioContext.sampleRate;
 	var sample_duration = 1/sample_rate;
 	var samples_per_strobe = Math.ceil(strobe_period/sample_duration);
+	var strobe_delta_t = 0;
+	var strobe_remainder = 0;
 
 	proc.onaudioprocess = function (e) {
 		// e's buffer is only valid for the duration of the callback, so copy it over
@@ -75,74 +77,80 @@ function init_strobe (canvas,stream,audioContext) {
 		canvas: canvas,
 		buffers: buffers,
 		processor: proc, // GC bug in Chrome
-		media_stream: media_stream, // GC bug in FireFox
+		stream: stream, // GC bug in FireFox
 		draw: function (raf_time ) {
+			if ( !buffer_t ) return; // no data yet
+
 			// calculate how much time the strobe has gone forward since the last frame
-			var frame_duration = ( raf_time - last_frame ) / 1000;
+			var frame_duration = last_frame ? ( raf_time - last_frame ) / 1000 : 0;
+			if ( frame_duration && frame_duration < strobe_period ) return; // plausible for bass or lower registers of piano
 
-			if ( frame_duration > strobe_period ) { // else case plausible for bass or low register of piano
-				last_frame = raf_time;
+			last_frame = raf_time;
 
-				// skip ahead 
-				strobe_delta_t += strobe_period * Math.floor( strobe_period / frame_duration );
+			// skip to the present, the remainder tracking is for better responsiveness
+			// underruns are normal at first and with smaller buffer sizes, but don't really matter since all we really care about is phase information
+			strobe_delta_t += strobe_period * Math.floor( (frame_duration + strobe_remainder) / strobe_period );
+			strobe_remainder = ( frame_duration + strobe_remainder ) % strobe_period;
+			while ( strobe_delta_t < 0 ) {
+				// console.log("underrun");
+				strobe_delta_t += strobe_period;
+			}
+			while ( strobe_delta_t * sample_rate + samples_per_strobe > 2 * buffers[0].length ) {
+				// console.log("overrun");
+				strobe_delta_t -= strobe_period;
+			}
 
-				// skip ahead frame duration plus enough strobe periods to be in the positive just in case
-				while ( strobe_delta_t < 0 ) {
-					strobe_delta_t += strobe_period;
+
+
+			// calculate the offset to the first sample of the strobe relative to the saved buffer
+			// Smooth will handle the fractional component of the offset
+			var offset = strobe_delta_t * sample_rate;
+			var offset_int = Math.floor(offset);
+			offset -= offset_int;
+
+			// Smooth needs a non typed array so copy over.
+			// fuck you javascript, why don't typed arrays support slice()?
+			var small_buf;
+			var bufer;
+			if ( offset_int + samples_per_strobe >= buffers[0].length && offset_int < buffers[0].length ) {
+				var diff = offset_int + samples_per_strobe - buffers[0].length;
+				// complex case of strobe sample intersecting with buffer pair boundary...
+				// we could just skip one more strobe period forward i suppose ;-)
+				small_buf = Array.prototype.slice.call(buffers[0], offset_int);
+				Array.prototype.push.apply( small_buf, buffers[1], 0, diff);
+			} else {
+				var buffer = buffers[0];
+
+				if ( offset_int >= buffers[0].length ) {
+					offset_int -= buffers[0].length;
+					buffer = buffers[1];
 				}
 
-				// calculate the offset to the first sample of the strobe relative to the saved buffer
-				// Smooth will handle the fractional component of the offset
-				var offset = strobe_delta_t * sample_rate;
-				var offset_int = Math.floor(offset);
-				offset -= offset_int;
+				small_buf = Array.prototype.slice.call( buffer, offset_int, offset_int + samples_per_strobe );
+			}
 
-				// Smooth needs a non typed array so copy over.
-				// fuck you javascript, why don't typed arrays support slice()?
+			if ( small_buf.length >= samples_per_strobe ) {
+				// otherwise low pitch = longer period = offset grows modulu period which means motion is to the right and by convention it goes to the left
+				small_buf.reverse();
 
-				var small_buf;
-				var bufer;
-				if ( offset_int + samples_per_strobe >= buffers[0].length && offset_int < buffers[0].length ) {
-					var diff = offset_int + samples_per_strobe - buffers[0].length;
-					// complex case of strobe sample intersecting with buffer pair boundary...
-					// we could just skip one more strobe period forward i suppose ;-)
-					small_buf = Array.prototype.slice.call(buffers[0], offset_int);
-					Array.prototype.push.apply( small_buf, buffers[1], 0, diff);
-				} else {
-					var buffer = buffers[0];
+				Smooth.deepValidation = false; // FIXME barf
+				var s = Smooth(small_buf); // FIXME config?
 
-					if ( offset_int >= buffers[0].length ) {
-						console.log("overrun?");
-						offset_int -= buffers[0].length;
-						buffer = buffers[1];
-					}
-
-					small_buf = Array.prototype.slice.call( buffer, offset_int, offset_int + samples_per_strobe );
-				}
-
-				if ( small_buf.length >= samples_per_strobe ) {
-					// otherwise low pitch = longer period = offset grows modulu period which means motion is to the right and by convention it goes to the left
-					small_buf.reverse();
-
-					Smooth.deepValidation = false; // FIXME barf
-					var s = Smooth(small_buf); // FIXME config?
-
-					// interpolate the buffer slice into the strobe segments
-					for (var i = 0; i < strobe_segments; i++) {
-						var v = s( offset + i * (samples_per_strobe / strobe_segments) );
-						var alpha = Math.floor(256 * (1+v)/2);
-						for (var col = i * seg_width; col < (i+1) * seg_width; col++) {
-							for ( var row = 0; row < strobe_height; row++ ) {
-								image_data.data[(row * strobe_width + col) * 4 + 3] = alpha;
-							}
+				// interpolate the buffer slice into the strobe segments
+				for (var i = 0; i < strobe_segments; i++) {
+					var v = s( offset + i * (samples_per_strobe / strobe_segments) );
+					var alpha = Math.floor(256 * (1+v)/2);
+					for (var col = i * seg_width; col < (i+1) * seg_width; col++) {
+						for ( var row = 0; row < strobe_height; row++ ) {
+							image_data.data[(row * strobe_width + col) * 4 + 3] = alpha;
 						}
 					}
-
-					canvasContext.putImageData(image_data,0,0);
-				} else {
-					// buffer overrun
-					console.log("OH SHIT");
 				}
+
+				canvasContext.putImageData(image_data,0,0);
+			} else {
+				// a real buffer overrun
+				console.log("OH SHIT");
 			}
 		}
 	};
